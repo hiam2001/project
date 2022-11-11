5fb5766a115d03e6a8da5274cf2eddffabf67f39
hiam2001 <flutterdart001@gmail.com>
2022-11-11 15:50:47 +0300

    pubspect packages

---
+++ lib/injection.dart
@@ -0,0 +1,21 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+// Features
+
+// Bloc
+
+// Usecases
+
+// Repository
+
+// Datasources
+
+// Core  sqflite / network info
+
+// External dio / sharedPreferences ..
+
+
+}
